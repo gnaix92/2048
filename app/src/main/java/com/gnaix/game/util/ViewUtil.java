c88dd2f181221b7b5687bcfa3a02fc97c9e46e9f
gnaix <990828785@qq.com>
2015-09-23 17:13:07 +0800

    complete UI

--- app/src/main/java/com/gnaix/game/util/ViewUtil.java
+++ app/src/main/java/com/gnaix/game/util/ViewUtil.java
@@ -1,6 +1,8 @@
 package com.gnaix.game.util;
 
+import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 
 /**
  * 名称: ViewUtil
@@ -18,5 +20,19 @@ public class ViewUtil {
      */
     public static int centerText(Paint paint){
         return (int) (paint.ascent() + paint.descent()) / 2;
+    }
+
+    /**
+     * 填充绘制区域
+     * @param canvas
+     * @param draw
+     * @param startX
+     * @param endX
+     * @param startY
+     * @param endY
+     */
+    public static void drawDrawable(Canvas canvas, Drawable draw, int startX, int startY, int endX, int endY){
+        draw.setBounds(startX, startY, endX, endY);
+        draw.draw(canvas);
     }
 }
--- app/src/main/java/com/gnaix/game/widget/MainView.java
+++ app/src/main/java/com/gnaix/game/widget/MainView.java
@@ -5,7 +5,9 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.support.v7.internal.widget.ViewUtils;
 import android.util.Log;
 import android.view.View;
 
@@ -24,55 +26,22 @@ public class MainView extends View {
     private final static String TAG = "MainView";
     //是否保存状态
     public static boolean hasSaveState = false;
-    //资源
-    private Resources mResources;
     //游戏模型
     private Game mGame;
 
+    //资源
+    private Resources mResources;
     //rectangle
     private Drawable backgroundRectangle;
     private Drawable lightUpRectangle;
-
-    //时间
-    private long lastFPSTime = System.nanoTime();
-    private long currentTiem = System.nanoTime();
-
-    public MainView(Context context) {
-        super(context);
-        Log.d(TAG, "MainView");
-
-        mResources = context.getResources();
-        mGame = new Game(context, this);
-
-        //加载资源
-        backgroundRectangle = mResources.getDrawable(R.drawable.background_rectangle);
-        lightUpRectangle = mResources.getDrawable(R.drawable.light_up_rectangle);
-
-        Log.d(TAG, "end MainView");
-    }
-
+    private Drawable fadeRectangle;
     private Bitmap background = null;
+
     /** 画笔 */
     private Paint mPaint = new Paint();
-
-    /**
-     * 页面大小变化时调用（初始化变量）
-     * @param w
-     * @param h
-     * @param oldw
-     * @param oldh
-     */
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh){
-        super.onSizeChanged(w,h,oldw, oldh);
-        Log.d(TAG, "start onSizeChanged");
-
-        getLayout(w, h);
-        createBitmapCells();
-        createBackgroundBitmp();
-
-        Log.d(TAG, "end onSizeChanged");
-    }
+    private int textColor_black;
+    private int textColor_while;
+    private int textColor_brown;
 
     /** 单元格大小 */
     private int cellSize;
@@ -114,6 +83,60 @@ public class MainView extends View {
     private int newGameStartX;
     private int undoStartX;
 
+    //时间
+    private long lastFPSTime = System.nanoTime();
+    private long currentTiem = System.nanoTime();
+
+
+
+    /**
+     * 构造函数
+     * @param context
+     */
+    public MainView(Context context) {
+        super(context);
+        Log.d(TAG, "MainView");
+
+        mResources = context.getResources();
+        mGame = new Game(context, this);
+
+        //加载资源
+        backgroundRectangle = mResources.getDrawable(R.drawable.background_rectangle);
+        lightUpRectangle = mResources.getDrawable(R.drawable.light_up_rectangle);
+        fadeRectangle = mResources.getDrawable(R.drawable.fade_rectangle);
+
+        textColor_black = mResources.getColor(R.color.text_black);
+        textColor_while = mResources.getColor(R.color.text_white);
+        textColor_brown = mResources.getColor(R.color.text_brown);
+
+        this.setBackgroundColor(mResources.getColor(R.color.background));
+
+        Typeface font = Typeface.createFromAsset(mResources.getAssets(), "ClearSans-Bold.ttf");
+        mPaint.setAntiAlias(true);
+        mPaint.setTypeface(font);
+
+        Log.d(TAG, "end MainView");
+    }
+
+    /**
+     * 页面大小变化时调用（初始化变量）
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w,h,oldw, oldh);
+        Log.d(TAG, "start onSizeChanged");
+
+        getLayout(w, h);
+        createBitmapCells();
+        createBackgroundBitmp(w, h);
+        createOverlays();
+
+        Log.d(TAG, "end onSizeChanged");
+    }
 
     /**
      * 计算控件布局坐标
@@ -136,6 +159,7 @@ public class MainView extends View {
 
         //设置画笔字体居中
         mPaint.setTextAlign(Paint.Align.CENTER);
+        mPaint.setTextSize(cellSize);
         // 其他字体大小
         textSize = cellSize * cellSize / Math.max(cellSize, mPaint.measureText("0000"));
         cellTextSize = textSize;
@@ -150,10 +174,10 @@ public class MainView extends View {
         //计算borad坐标
         double halfNumSquaresX = Game.numSquaresX / 2d;
         double halfNumSquaresY = Game.numSquaresY / 2d;
-        startingX = (int) (boardMiddleX - (cellSize + gridWidth) * halfNumSquaresX + gridWidth / 2);
-        endingX = (int) (boardMiddleX + (cellSize + gridWidth) * halfNumSquaresX - gridWidth / 2);
-        startingY = (int) (boardMiddleY -(cellSize + gridWidth) * halfNumSquaresY + gridWidth /2);
-        endingY = (int) (boardMiddleY + (cellSize + gridWidth) * halfNumSquaresY - gridWidth /2);
+        startingX = (int) (boardMiddleX - (cellSize + gridWidth) * halfNumSquaresX - gridWidth / 2);
+        endingX = (int) (boardMiddleX + (cellSize + gridWidth) * halfNumSquaresX + gridWidth / 2);
+        startingY = (int) (boardMiddleY -(cellSize + gridWidth) * halfNumSquaresY - gridWidth /2);
+        endingY = (int) (boardMiddleY + (cellSize + gridWidth) * halfNumSquaresY + gridWidth /2);
 
         //设置title字体大小
         mPaint.setTextSize(titleTextSize);
@@ -180,8 +204,142 @@ public class MainView extends View {
         Log.d(TAG, "end getLayout, time:" + lastFPSTime);
     }
 
+    /**
+     * 绘制单元格
+     */
     private void createBitmapCells(){
-        
+
+    }
+
+    /**
+     * 绘制页面背景
+     * @param width
+     * @param height
+     */
+    private void createBackgroundBitmp(int width, int height){
+        background = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(background);
+
+        drawHeader(canvas);
+        drawNewGameButton(canvas, false);
+        drawUndoButton(canvas);
+        drawBackground(canvas);
+        drawBackgroundGrid(canvas);
+        drawInstructions(canvas);
+    }
+
+    /**
+     * 绘制title
+     * @param canvas
+     */
+    private void drawHeader(Canvas canvas){
+        mPaint.setTextSize(headerTextSize);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+        mPaint.setColor(textColor_black);
+        int textShiftY = ViewUtil.centerText(mPaint) * 2;
+        int headerStartY = startYAll - textShiftY;
+        canvas.drawText(mResources.getString(R.string.header), startingX, headerStartY, mPaint);
+
+    }
+
+    /**
+     * 绘制重新开始按钮
+     * @param canvas
+     * @param lightUp
+     */
+    private void drawNewGameButton(Canvas canvas, boolean lightUp){
+        if(lightUp){
+            ViewUtil.drawDrawable(
+                    canvas,
+                    lightUpRectangle,
+                    newGameStartX,
+                    iconStartYAll,
+                    newGameStartX + iconSize,
+                    iconStartYAll + iconSize);
+        }else{
+            ViewUtil.drawDrawable(
+                    canvas,
+                    backgroundRectangle,
+                    newGameStartX,
+                    iconStartYAll,
+                    newGameStartX + iconSize,
+                    iconStartYAll + iconSize);
+        }
+
+        ViewUtil.drawDrawable(
+                canvas,
+                mResources.getDrawable(R.drawable.ic_action_refresh),
+                newGameStartX + iconPaddingSize,
+                iconStartYAll + iconPaddingSize,
+                newGameStartX +iconSize - iconPaddingSize,
+                iconStartYAll +iconSize - iconPaddingSize);
+    }
+
+    /**
+     * 绘制返回按钮
+     * @param canvas
+     */
+    private void drawUndoButton(Canvas canvas){
+        ViewUtil.drawDrawable(
+                canvas,
+                backgroundRectangle,
+                undoStartX,
+                iconStartYAll,
+                undoStartX + iconSize,
+                iconStartYAll + iconSize);
+
+        ViewUtil.drawDrawable(
+                canvas,
+                mResources.getDrawable(R.drawable.ic_action_undo),
+                undoStartX + iconPaddingSize,
+                iconStartYAll + iconPaddingSize,
+                undoStartX +iconSize - iconPaddingSize,
+                iconStartYAll +iconSize - iconPaddingSize);
+    }
+
+    /**
+     * 绘制borad
+     * @param canvas
+     */
+    private void drawBackground(Canvas canvas){
+        ViewUtil.drawDrawable(canvas, backgroundRectangle, startingX, startingY, endingX, endingY);
+    }
+
+    /**
+     * 绘制单元格边框
+     * @param canvas
+     */
+    private void drawBackgroundGrid(Canvas canvas) {
+        Drawable cellRectangle = mResources.getDrawable(R.drawable.cell_rectangle);
+
+        for (int i = 0; i < Game.numSquaresX; i++) {
+            for (int j = 0; j < Game.numSquaresY; j++) {
+                int sX = startingX + gridWidth + (cellSize + gridWidth) * i;
+                int eX = sX + cellSize;
+                int sY = startingY + gridWidth + (cellSize + gridWidth) * j;
+                int eY = sY + cellSize;
+
+                ViewUtil.drawDrawable(
+                        canvas,
+                        cellRectangle,
+                        sX,
+                        sY,
+                        eX,
+                        eY);
+            }
+        }
+    }
+
+    /**
+     * 介绍栏
+     * @param canvas
+     */
+    private void drawInstructions(Canvas canvas){
+        mPaint.setTextSize(instructionsTextSize);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+        int textShiftY = ViewUtil.centerText(mPaint)*2;
+
+       canvas.drawText(mResources.getString(R.string.instructions), startingX, endingY + textPaddingSize - textShiftY , mPaint);
     }
 
 
